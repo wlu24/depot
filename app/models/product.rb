49787dfe22ff369018ad3ed9505d97c9d1ccc681
Wayne Lu <waynelu24lon@gmail.com>
2018-01-04 23:17:08 +0000

    Task D: Cart Creation
    Iteration D1: Finding a Cart
    Iteration D2: Connecting Products to Carts
    Iteration D3: Adding a Button

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+
   validates :title, :description, :image_url, presence: true
 
   # Test against one cent rather than zero because the database stores just two
@@ -6,7 +11,7 @@ class Product < ApplicationRecord
   # even though it would pass the validation if we compared against zero.
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, length: {minimum: 10}
 
   # Check that the URL ends with one of .gif, .jpg, or .png
   #
@@ -15,4 +20,15 @@ class Product < ApplicationRecord
   validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
+
+
+  private
+
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+      end
+    end
 end
