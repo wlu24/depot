bc9b33d4b9a2ca03dd83198008bbf1153e92ffac
Wayne Lu <waynelu24lon@gmail.com>
2018-01-12 12:18:12 +0000

    Implement Pago, a slow payment processor simulator
    Simulate the payment process by calling sleep() and making the processor
    respond to succeeded?() by using OpenStruct.

---
+++ lib/pago.rb
@@ -0,0 +1,34 @@
+require 'ostruct'
+class Pago
+  def self.make_payment(order_id:,
+                        payment_method:,
+                        payment_details:)
+
+    case payment_method
+    when :check
+      Rails.logger.info "Processing check: " +
+        payment_details.fetch(:routing).to_s + "/" +
+        payment_details.fetch(:account).to_s
+    when :credit_card
+      Rails.logger.info "Processing credit_card: " +
+        payment_details.fetch(:cc_num).to_s + "/" +
+        payment_details.fetch(:expiration_month).to_s + "/" +
+        payment_details.fetch(:expiration_year).to_s
+    when :po
+      Rails.logger.info "Processing purchase order: " +
+        payment_details.fetch(:po_num).to_s
+    else
+      raise "Unknown payment_method #{payment_method}"
+    end
+    sleep 3 unless Rails.env.test?
+    Rails.logger.info "Done Processing Payment"
+
+
+    # Part of Ruby’s standard library and provides a quick-and-dirty way to
+    # make an object that responds to the methods given to its constructor.
+    #
+    # OpenStruct is handy for creating realistic objects from prototype or
+    # faked-out code like Pago.
+    OpenStruct.new(succeeded?: true)
+  end
+end
