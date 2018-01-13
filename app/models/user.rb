82451a6c6e3ee16c9dc7bc0a3e238b724d5498d0
Wayne Lu <waynelu24lon@gmail.com>
2018-01-13 19:40:06 +0000

    Iter J4: Adding a Sidebar, More Administration

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,26 @@ class User < ApplicationRecord
   # This line was added because we specified password:digest when we run
   # $ rails generate scaffold User name:string password:digest
   has_secure_password
+
+
+
+  # ActiveRecord hook method. Called after the SQL delete is executed.
+  after_destroy :ensure_an_admin_remains
+
+  class Error < StandardError
+  end
+
+   private
+     def ensure_an_admin_remains
+       if User.count.zero?
+         # This exception serves two purposes. First, because it’s raised inside
+         # a transaction, it causes an automatic rollback. By raising the
+         # exception if the users table is empty after the deletion, we undo the
+         # delete and restore that last user. Second, the exception signals the
+         # error back to the controller, where we use a rescue_from block to
+         # handle it and report the error to the user in the notice.
+         raise Error.new "Can't delete last user"
+       end
+     end
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,10 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  # Add more helper methods to be used by all tests here...
+end
+
+class ActionDispatch::IntegrationTest
   def login_as(user)
     post login_url, params: { name: user.name, password: 'secret' }
   end
