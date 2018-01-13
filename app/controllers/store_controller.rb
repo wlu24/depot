c5491a31101c18dcf0424d4ab5dd2ab4a88c09b3
Wayne Lu <waynelu24lon@gmail.com>
2018-01-13 11:15:23 +0000

    Iter J3: Limiting Access
    Use callbacks, e.g. before_action() and skip_before_action(), to
    require admin login for admin pages.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,9 @@
 class StoreController < ApplicationController
   include CurrentCart
   before_action :set_cart
-  
+
+  skip_before_action :authorize
+
   def index
     @products = Product.order(:title)
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,5 +5,15 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def login_as(user)
+    post login_url, params: { name: user.name, password: 'secret' }
+  end
+
+  def logout
+    delete logout_url
+  end
+
+  def setup
+    login_as users(:one)
+  end
 end
